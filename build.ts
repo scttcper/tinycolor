ba52ce85596e1707290a1910aa7974e29c828e7c
Scott Cooper <scttcper@gmail.com>
2018-04-28 15:44:07 -0700

    build: remove tslib

--- build.ts
+++ build.ts
@@ -6,16 +6,12 @@ const sourceMaps = require('rollup-plugin-sourcemaps');
 
 const moduleInputOptions: RollupFileOptions = {
   input: `dist/esm5/public_api.js`,
-  external: ['tslib'],
   plugins: [sourceMaps()],
 };
 const moduleOutputOptions: OutputOptions = {
   file: './dist/package-dist/bundles/tinycolor-3.es2015.js',
   format: 'es',
   name: 'tinycolor3',
-  globals: {
-    tslib: 'tslib',
-  },
   sourcemap: true,
 };
 

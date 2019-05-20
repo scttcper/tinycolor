15338083836a1c2f29d688e928b9fe72fa0a915a
Scott Cooper <scttcper@gmail.com>
2019-05-20 00:36:11 -0700

    feat: allow install from github

--- build.ts
+++ build.ts
@@ -1,6 +1,3 @@
-import { join } from 'path';
-
-import { copySync } from 'fs-extra';
 import { rollup, OutputOptions, RollupOptions } from 'rollup';
 import { terser } from 'rollup-plugin-terser';
 import sourceMaps from 'rollup-plugin-sourcemaps';
@@ -22,13 +19,6 @@ async function build() {
   // create browser bundle
   const umdMin = await rollup(umdMinInputOptions);
   await umdMin.write(umdMinOutputOptions);
-
-  // copy git folder to dist folder for semantic-release
-  copySync('.git', join(process.cwd(), 'dist/.git'));
-  // copy files to distribution folder
-  copySync('package.json', join(process.cwd(), 'dist/package.json'));
-  copySync('README.md', join(process.cwd(), 'dist/README.md'));
-  copySync('LICENSE', join(process.cwd(), 'dist/LICENSE'));
 }
 
 build()

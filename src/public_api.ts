f16e4a51ddba430be38192588ba39ff3b7126d42
07akioni <07akioni2@gmail.com>
2021-02-10 01:46:39 +0800

    feat: export conversion interface

--- src/public_api.ts
+++ src/public_api.ts
@@ -7,6 +7,7 @@ export * from './from-ratio';
 export * from './format-input';
 export * from './random';
 export * from './interfaces';
+export * from './conversion';
 
 // kept for backwards compatability with v1
 export default tinycolor;
